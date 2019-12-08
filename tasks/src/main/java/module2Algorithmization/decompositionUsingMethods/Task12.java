15f9d4b2ce3a8097beefc0fce4783bcb41b48585
sergeiAmko <Cthutq..1996..Vbijnf>
2019-12-08 16:18:27 +0300

    update module 2. DecompositionUsingMethod. Task 12

--- tasks/src/main/java/module2Algorithmization/decompositionUsingMethods/Task12.java
+++ tasks/src/main/java/module2Algorithmization/decompositionUsingMethods/Task12.java
@@ -1,17 +1,60 @@
 package module2Algorithmization.decompositionUsingMethods;
 
 
+import java.util.Arrays;
+
+import static module2Algorithmization.Service.*;
+
 /**
  * Даны натуральные числа K и N. Написать метод (методы) формирования массива А,
- * элементами которого являются числа, сумма цифр которых равна K и которые не больше N/
+ * элементами которого являются числа, сумма цифр которых равна K и которые не больше N
  */
 public class Task12 {
 
 
-//    private static int[] makeArray(long k, long n){
-//
-//
-//
-//    }
+    public static void main(String[] args) {
+        int k, n;
+        String s = "Input number";
+
+        k = getPositiveIntValue(s + " k");
+        n = getPositiveIntValue(s + " n");
+
+        System.out.println("Arrays:");
+        System.out.println(Arrays.toString(createK(k, n)));
+    }
+
+    private static int[] createK(int k, int n) {
+
+        int[] outWithZero = new int[n];
+        int[] out;
+        int[] middle;
+        int count = 0;
+
+        if (k < 1 && n < 1) {
+            return null;
+        }
+
+        for (int i = 1; i < n; i++) {
+            int sum = 0;
+            middle = getDigitsOfNumber(i);
+
+            for (int value : middle) {
+                sum += value;
+            }
+
+            if (sum == k) {
+                outWithZero[count] = i;
+                count++;
+            }
+
+
+        }
+
+        out = new int[count];
+
+        System.arraycopy(outWithZero, 0, out, 0, out.length);
+
+        return out;
+    }
 
 }
